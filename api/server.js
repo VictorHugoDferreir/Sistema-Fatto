ec181948057a23021d2f1fc79e13de4f595dc2dc
Victuhdf7 <167028759+VictorHugoDferreir@users.noreply.github.com>
2024-11-21 09:30:39 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 
-// Roteamento de arquivos estáticos
+//roteamento de arquivos estáticos
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/html/index.html");
 });
@@ -23,31 +23,30 @@ app.get("/script.js", (req, res) => {
   res.sendFile(__dirname + "/html/script.js");
 });
 
-// Cadastrar uma nova tarefa
+//cadastrando uma nova tarefa
 app.post('/tarefas', async (req, res) => {
   try {
-    // Criando a tarefa no banco de dados usando o Prisma
+    //criando a tarefa no banco de dados usando o Prisma
     const tarefaCriada = await prisma.task.create({
       data: {
         nome: req.body.nome,
-        custo: parseFloat(req.body.custo),  // Certifique-se de que o custo seja convertido para float
+        custo: parseFloat(req.body.custo), 
         dataLimite: new Date(req.body.dataLimite)
       }
     });
-
-    // Retornando a tarefa criada com o ID gerado pelo banco de dados
+    
     res.status(201).json(tarefaCriada);
 
-  } catch (error) {
-    console.error('Erro ao criar tarefa:', error);
-    res.status(500).json({ error: 'Erro ao criar tarefa' });
-  }
-});
+  } 
+    catch(error){
+      console.error('Erro ao criar tarefa:', error);
+      res.status(500).json({ error: 'Erro ao criar tarefa' });
+    }
+  });
 
-// Listar todas as tarefas
+//listar as tarefas
 app.get('/tarefas', async (req, res) => {
   try {
-    // Usando o Prisma para buscar todas as tarefas no banco de dados
     const tarefas = await prisma.task.findMany();
     res.status(200).json(tarefas);
   } catch (error) {
